85bb51fb3dcdd1c81df8bb2c0a661c3e7647a596
Mayank-3e <7mayank.agarwal@gmail.com>
2022-12-17 15:09:53 +0530

    updated link

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-const socket=io('https://ichat-app-mi9v1fiv7-mayank-3e.vercel.app/')
+const socket=io('https://ichat-app-api.vercel.app/')
 const form=document.getElementById('send-container');
 const messageInput=document.getElementById('messageInp');
 const messageContainer=document.querySelector('.container');
