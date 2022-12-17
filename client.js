dc6e6f9172ce9e1196927d095d0590758599b4a7
Mayank-3e <7mayank.agarwal@gmail.com>
2022-12-17 13:02:38 +0530

    trying: io(localhost)

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-const socket=io('https://ghastly-spirit-14498.herokuapp.com/');
+const socket=io('http://localhost:'+process.env.PORT+'/');
 const form=document.getElementById('send-container');
 const messageInput=document.getElementById('messageInp');
 const messageContainer=document.querySelector('.container');
